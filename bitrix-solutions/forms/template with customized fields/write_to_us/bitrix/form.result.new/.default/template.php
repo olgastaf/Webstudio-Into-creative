dda31910d067c752419fec7cb9ba7d0fbf8e0f2f
olgastaf <olga-staf@yandex.ru>
2026-09-18 14:36:53 +0700

    Fix nested form in Bitrix form template

--- bitrix-solutions/forms/template with customized fields/write_to_us/bitrix/form.result.new/.default/template.php
+++ bitrix-solutions/forms/template with customized fields/write_to_us/bitrix/form.result.new/.default/template.php
@@ -12,8 +12,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
     <?=$arResult["FORM_HEADER"] // Здесь появляется сообщение об ошибках ?>
 
-    <form class="page__form" name="<?=$arResult["WEB_FORM_NAME"]?>" action="<?=POST_FORM_ACTION_URI?>" method="POST" enctype="multipart/form-data">
-        <?=bitrix_sessid_post();?>
+            <?=bitrix_sessid_post();?>
         <input type="hidden" name="WEB_FORM_ID" value="<?=$arParams["WEB_FORM_ID"]?>">
         <input type="hidden" name="web_form_submit" value="Y">
 
@@ -123,10 +122,6 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
                 value="<?=htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]);?>"
             />
         </div>
-
-
-    </form>
-
-    <?//=$arResult["FORM_FOOTER"]?>
+    <?=$arResult["FORM_FOOTER"]?>
 
 <?endif;?>
